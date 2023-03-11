608a8af9527697faec889ce81188365fe4459fa8
Matthias Weis <matthias1.weis@web.de>
2023-03-11 08:58:59 +0100

    add get version info

--- src/sc_inetbox_adapter/sc_inetbox_adapter.py
+++ src/sc_inetbox_adapter/sc_inetbox_adapter.py
@@ -9,8 +9,7 @@ class Internetbox_Adapter:
         self._protocol = protocol
         self._ip_address = ip_address
 
-    def get_router_data(self):
-        url = "%s://%s/ws" % (self._protocol, self._ip_address)
+    def get_devices(self):
 
         payload = json.dumps({
         "service": "Devices",
@@ -20,10 +19,20 @@ class Internetbox_Adapter:
             "flags": "no_actions"
         }
         })
+
+        response = self._send_ws_request(payload)
+
+    def get_software_version(self):
+        payload = json.dumps({"service":"APController","method":"getSoftWareVersion","parameters":{}})
+        self._send_ws_request(payload)
+
+
+    def _send_ws_request(self, payload):
+        url = "%s://%s/ws" % (self._protocol, self._ip_address)
         headers = {
-        'Content-Type': 'application/x-sah-ws-4-call+json'
+        'Content-Type': 'application/x-sah-ws-4-call+json; charset=UTF-8'
         }
         urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-        response = requests.request("POST", url, headers=headers, data=payload, verify=False)
-
+        response =  requests.request("POST", url, headers=headers, data=payload, verify=False)
         print(response.text)
+        return response
--- tests/test_sc_inetbox_adapter.py
+++ tests/test_sc_inetbox_adapter.py
@@ -1,5 +1,9 @@
 from sc_inetbox_adapter.sc_inetbox_adapter import Internetbox_Adapter
 
-def test_answer():
+def test_devices():
     ad = Internetbox_Adapter()
-    ad.get_router_data()
+    ad.get_devices()
+
+def test_version():
+    ad = Internetbox_Adapter()
+    ad.get_software_version()
