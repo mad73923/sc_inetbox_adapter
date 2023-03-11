726de3decbe8ef8bef674e17b06b44fc0adb93cb
Matthias Weis <matthias1.weis@web.de>
2023-03-11 10:03:24 +0100

    add context; add pytest fixture

--- src/sc_inetbox_adapter/sc_inetbox_adapter.py
+++ src/sc_inetbox_adapter/sc_inetbox_adapter.py
@@ -5,9 +5,18 @@ import urllib3
 
 class Internetbox_Adapter:
 
-    def __init__(self, protocol: str = "https", ip_address: str = "192.168.1.1") -> None:
+    def __init__(self, admin_password: str, protocol: str = "https", ip_address: str = "192.168.1.1") -> None:
+        self._admin_password = admin_password
         self._protocol = protocol
         self._ip_address = ip_address
+
+    def create_context(self):
+        payload = json.dumps({"service":"sah.Device.Information","method":"createContext","parameters":{"applicationName":"webui","username":"admin","password":self._admin_password}})
+        headers = {
+            'Authorization': 'X-Sah-Login',
+        }
+        response = self._send_ws_request(payload, headers)
+        print(response.status_code)
 
     def get_devices(self):
 
@@ -27,11 +36,9 @@ class Internetbox_Adapter:
         self._send_ws_request(payload)
 
 
-    def _send_ws_request(self, payload):
+    def _send_ws_request(self, payload: str, headers={}):
         url = "%s://%s/ws" % (self._protocol, self._ip_address)
-        headers = {
-        'Content-Type': 'application/x-sah-ws-4-call+json; charset=UTF-8'
-        }
+        headers['Content-Type'] = 'application/x-sah-ws-4-call+json'
         urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
         response =  requests.request("POST", url, headers=headers, data=payload, verify=False)
         print(response.text)
--- tests/test_sc_inetbox_adapter.py
+++ tests/test_sc_inetbox_adapter.py
@@ -1,9 +1,16 @@
+import pytest
 from sc_inetbox_adapter.sc_inetbox_adapter import Internetbox_Adapter
 
-def test_devices():
-    ad = Internetbox_Adapter()
-    ad.get_devices()
+@pytest.fixture
+def inetbox():
+    with open(".password") as pwd_file:
+        return Internetbox_Adapter(pwd_file.readline())
 
-def test_version():
-    ad = Internetbox_Adapter()
-    ad.get_software_version()
+def test_get_context(inetbox):
+    inetbox.create_context()
+
+def test_devices(inetbox):
+    inetbox.get_devices()
+
+def test_version(inetbox):
+    inetbox.get_software_version()
