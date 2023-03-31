4786a4757fd0d61b16121014b2890f4259ff7e1a
Matthias Weis <matthias1.weis@web.de>
2023-03-31 14:51:25 +0200

    add default value to package

--- src/sc_inetbox_adapter/__init__.py
+++ src/sc_inetbox_adapter/__init__.py
@@ -3,3 +3,4 @@ from importlib.metadata import version
 __version__ = version("sc_inetbox_adapter")
 
 from .sc_inetbox_adapter import InternetboxAdapter
+from .const import DEFAULT_HOST, DEFAULT_SSL
